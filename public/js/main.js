97d894827999681660dc05eb8ebd4e688cac2436
Hugo <becker.hugo@hotmail.com>
2020-12-17 12:59:00 +0100

    S2

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,82 @@
 // SELECTOR
 todoInput=document.querySelector('.todo-input');
 addTodoButton=document.querySelector('.add-todo');
+todoList=document.querySelector('.todo-list')
+todoDiv=document.querySelector('.todo-div')
+
+
 
 
 // EVENTLISTENER
-addTodoButton.addEventListener('click',addTodoFunction)
+// add todo
+addTodoButton.addEventListener('click',addTodoFunction);
+// check todo
+todoList.addEventListener('click',CheckEditDeleteFunction)
 
 
-// FUNCTION
+
+
+
+// FUNCTIONS
+
+// add to do
 function addTodoFunction(event) {
-    console.log('yoo');
+    // DIV
+    let todoDiv=document.createElement('div')
+    todoDiv.classList.add('todo-div')
+    todoList.appendChild(todoDiv)
+    // LI
+    let todoLi=document.createElement('li')
+    todoLi.innerText=todoInput.value
+    todoLi.classList.add('todo-li')
+    todoDiv.appendChild(todoLi)
+    // CHECK BUTTON
+    let todoCheck=document.createElement('button')
+    todoCheck.innerHTML='<i class="fas fa-check-circle"></i>'
+    todoCheck.classList.add('todo-check')
+    todoDiv.appendChild(todoCheck)
+    // EDIT BUTTON
+    let todoEdit=document.createElement('button')
+    todoEdit.innerHTML='<i class="far fa-edit"></i>'
+    todoEdit.classList.add('todo-edit')
+    todoDiv.appendChild(todoEdit)
+    // DELETE BUTTON
+    let todoDelete=document.createElement('button')
+    todoDelete.innerHTML='<i class="far fa-trash-alt"></i>'
+    todoDelete.classList.add('todo-delete')
+    todoDiv.appendChild(todoDelete)
 
+    // empty onput
+    todoInput.value=""
+
+
+}
+
+
+function CheckEditDeleteFunction(e) {
+
+    let item=e.target; 
+    // console.log(item);
+
+    if(item.classList[0]==='todo-delete'){
+        let todo=item.parentElement
+        todo.remove()
+    }
+
+    if(item.classList[0]=='todo-check'){
+
+        let todo=item.parentElement
+        let li = todo.querySelector('.todo-li')
+        li.innerText.style.textDecoration='line-through'
+
+
+
+
+    }
+
+
+     
     
 }
+
+
