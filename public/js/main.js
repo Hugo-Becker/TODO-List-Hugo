5c0ee8586ac13ff639e9f25206f5c267d4da3548
Hugo <becker.hugo@hotmail.com>
2020-12-17 10:38:21 +0100

    S1

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+// SELECTOR
+todoInput=document.querySelector('.todo-input');
+addTodoButton=document.querySelector('.add-todo');
+
+
+// EVENTLISTENER
+addTodoButton.addEventListener('click',addTodoFunction)
+
+
+// FUNCTION
+function addTodoFunction(event) {
+    console.log('yoo');
+
+    
+}
