5ffd90bd355a297fb1ba30e95743ab83e09395df
Hugo <becker.hugo@hotmail.com>
2020-12-18 11:07:31 +0100

    S3

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ let addTodoButton=document.querySelector('.add-todo');
 let todoList=document.querySelector('.todo-list')
 let todoDiv=document.querySelectorAll('.todo-div')
 let editCount=0
+let checkCount=0
 let stillTodo=document.querySelector('.still-todo')
 let done=document.querySelector('.done')
 let seeAll=document.querySelector('.see-all')
@@ -47,7 +48,7 @@ function addTodoFunction(event) {
         // DIV
         let todoDiv=document.createElement('div')
         todoDiv.classList.add('todo-div','current')
-        todoList.appendChild(todoDiv)
+        todoList.prepend(todoDiv)
         // LI
         let todoLi=document.createElement('li')
         todoLi.innerText=todoInput.value
@@ -82,26 +83,69 @@ function addTodoFunction(event) {
 function CheckEditDeleteFunction(e) {
 
     let item=e.target; 
-    // console.log(item);
+    console.log(item);
+    let todo=item.parentElement
+    let edit=todo.querySelector('.todo-edit')
+    console.log(edit);
+    console.log(edit.classList.contains('invisible'));
 
+
+
+    // DELETE
     if(item.classList[0]==='todo-delete'){
         let todo=item.parentElement
-        todo.remove()
-    }
+        todo.classList.add('fade')
+        todo.addEventListener('transitionend',function(){
 
+            todo.remove()
+        })
+    }
+    // CHECK
     if(item.classList[0]==='todo-check'){
-
         let todo=item.parentElement
-        todo.classList.replace('current','checked')
+     
+            
+            if (todo.classList[1]!='checked') {
+                let todo=item.parentElement
+                todo.classList.replace('current','checked')
+            }
+            // uncheck
+            else if(todo.classList[1]==='checked'){
+                todo.classList.replace('checked','current')
+    
+    
+            }
         
-    }
+        // check
 
+
+
+
+
+        
+    }   
+
+     
+
+
+    // EDIT
     if(item.classList[0]==='todo-edit'&&editCount==0){
         editCount=1
         const todo=item.parentElement
+        var check=todo.querySelector('.todo-check')
         const li=todo.querySelector('.todo-li')
-        li.style.display='none'
+        const edit=todo.querySelector('.todo-edit')
+        const del=todo.querySelector('.todo-delete')
         const inputEdit=document.createElement('input')
+        let newCheck=document.createElement('button')
+        newCheck.innerHTML='<i class="fas fa-check-circle"></i>'
+        todo.insertBefore(newCheck,edit)
+        newCheck.classList.add('todo-newcheck')
+        edit.classList.add('invisible')
+        del.classList.add('invisible')
+        check.classList.add('display-None')
+        todo.classList.add('anim')
+        li.style.display='none'
         inputEdit.classList.add('edit-input')
         inputEdit.type='text'
         inputEdit.value=li.innerText
@@ -109,12 +153,32 @@ function CheckEditDeleteFunction(e) {
         todo.insertBefore(inputEdit,li)
         // validate change
         if(li.style.display==='none'){
-            item.addEventListener('dblclick',function(){
+            const todo=item.parentElement
+            todo.addEventListener('keypress',function(e){
+                if (e.key==='Enter') {
+                    todo.classList.remove('anim')
+                    edit.classList.remove('invisible')
+                    del.classList.remove('invisible')
+                    check.classList.remove('display-None')
+                    li.style.display='block'
+                    li.innerText=inputEdit.value
+                    inputEdit.remove()
+                    editCount=0
+                    newCheck.remove()
+                }
+            })
+            newCheck.addEventListener('click',()=>{
+                todo.classList.remove('anim')
+                edit.classList.remove('invisible')
+                del.classList.remove('invisible')
+                check.classList.remove('display-None')
                 li.style.display='block'
                 li.innerText=inputEdit.value
                 inputEdit.remove()
                 editCount=0
-                })
+                newCheck.remove()
+
+            })
         }
         
     }
